294cdfb18a0c84401d1e51de53bef24b5d76d4ec
renzrivero <zner26@gmail.com>
2024-05-06 21:32:51 -0600

    added double jump + higher platforms for it

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+const MAX_JUMP_COUNT = 2
+var jump_count = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -21,8 +23,11 @@ func _physics_process(delta):
 		coyote_timer.start()
 
 	# Allow jump only if on floor or the coyote timer is running
-	if Input.is_action_just_pressed("jump") and (is_on_floor() or !coyote_timer.is_stopped()):
-		velocity.y = JUMP_VELOCITY
+	# if Input.is_action_just_pressed("jump") and (is_on_floor() or !coyote_timer.is_stopped()):
+	if Input.is_action_just_pressed("jump") and jump_count < 1:
+		if (is_on_floor() or !coyote_timer.is_stopped()):
+			velocity.y = JUMP_VELOCITY
+			jump_count += 1
 
 	# Get the input direction: -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
@@ -33,8 +38,9 @@ func _physics_process(delta):
 	elif direction < 0:
 		animated_sprite.flip_h = true
 		
-	# Play animations
+	# Play animations reset jump count
 	if is_on_floor():
+		jump_count = 0
 		if direction == 0:
 			animated_sprite.play("Idle")
 		else:
